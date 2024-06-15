15080d21eecce5e4d20991dd294449436f7edd5a
Kane Bernard <kane.bernard@contractor.brightinsight.com>
2024-06-15 00:44:59 -0400

    Added Files from Branch New Project

--- App.tsx
+++ App.tsx
@@ -1,36 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { AuthenticationContext, AuthenticationContextObject } from './src/context/AuthenticationContext';
-import Routes from './src/routes';
-import { getFromStorage, removeFromStorage, setInStorage } from './src/services/storage';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    const [username, setUsername] = useState<string | null>(null);
-    const [initialRouteName, setInitialRouteName] = useState<string>();
-
-    const authenticationContextObj: AuthenticationContextObject = {
-        value: username,
-        setValue: (username) => {
-            setUsername(username);
-            if (username) {
-                setInStorage('currentUser', username);
-            } else {
-                removeFromStorage('currentUser');
-            }
-        },
-    };
-
-    useEffect(() => {
-        getFromStorage<string>('currentUser')
-            .then((storedUser) => {
-                setUsername(storedUser);
-                setInitialRouteName('Main');
-            })
-            .catch(() => setInitialRouteName('Setup'));
-    }, []);
-
-    return (
-        <AuthenticationContext.Provider value={authenticationContextObj}>
-            {initialRouteName && <Routes initialRouteName={initialRouteName} />}
-        </AuthenticationContext.Provider>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/context/AuthenticationContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-export type AuthenticationContextObject = {
-    value: string | null;
-    setValue: (newValue: string | null) => void;
-};
-
-export const AuthenticationContext = createContext<AuthenticationContextObject | null>(null);
